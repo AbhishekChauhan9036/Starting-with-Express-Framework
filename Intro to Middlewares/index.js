873ab370c720bdd09e24063b01aa19e3483d95ed
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-07 17:04:44 +0530

    Intro to Middleware

---
+++ Intro to Middlewares/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./route.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', route);
+
+app.listen(3000, function() {
+    console.log('Express app running on port ' + 3000)
+});
+        
