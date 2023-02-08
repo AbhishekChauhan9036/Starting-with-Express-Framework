82808a993eb0077045c39745bfcef32cc34ae798
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-08 15:33:14 +0530

    Intro to middleware

--- Intro to Middlewares/index.js
+++ Intro to Middlewares/index.js
@@ -1,16 +1,24 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-var bodyParser = require('body-parser');
+const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-const route = require('./route.js');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/', route);
-
-app.listen(3000, function() {
-    console.log('Express app running on port ' + 3000)
+app.use('/',(req,res,next)=>{
+    console.log('This Always Run!');
+    next();
 });
-        
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="post"><label for="fname">First name:</label><br><input type="text" id="fname" name="fname"><br><label for="lname">Last name:</label><br> <input type="text" id="lname" name="lname"> </form> <input type="submit" value="Submit">');
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+    console.log("the add product page");
+    res.send('<h1> this is Express! </h1>');
+});
+
+app.listen(3000);
