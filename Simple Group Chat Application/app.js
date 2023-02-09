9eaf6649cef8479abfedd05711138733f27cd881
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-09 13:23:13 +0530

    Simple Group Chat Application

--- Simple Group Chat Application/app.js
+++ Simple Group Chat Application/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/login", (req, res, next) => {
   res.send(
