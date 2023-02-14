258a40c09b73cd71e53a18d6158f615df73ba709
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-14 13:39:23 +0530

    Serving HTML CSS and JS VIEWS

--- Serving HTML CSS and JS VIEWS/routes/admin.js
+++ Serving HTML CSS and JS VIEWS/routes/admin.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const path=require('path')
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="post"><input type="text" name="title"><button type=sumbit> Add Product </form>'
-  );
+//   res.send(
+//     '<form action="/product" method="post"><input type="text" name="title"><button type=sumbit> Add Product </form>'
+//   );
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
 });
 
 router.post("/product", (req, res, next) => {
